0c0e68d2d77883632e54ce5d6a60b8d5abbcc3e7
Hemanth Kumar <avisihks@gmail.com>
2023-10-21 11:40:33 +0530

    Add validations

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,35 +2,59 @@ const User = require("../models/User");
 
 const createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const { body } = req;
 
-    res.status(201).json(user);
+    // Input Validation
+    if (!body) {
+      return res.status(400).json({ message: "Invalid input" });
+    }
+
+    const user = await User.create(body);
+    return res.status(201).json(user);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    console.error(error);
+
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
   }
 };
 
 const getUsers = async (req, res) => {
   try {
     const users = await User.findAll();
-    res.status(200).json(users);
+    return res.status(200).json(users);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    console.error(error);
+
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
   }
 };
 
-const getUser = async (req, res) => {
-  const userId = req.params.userId;
+const getUserByUserId = async (req, res) => {
   try {
+    const { userId } = req.params;
+
+    // Input Validation
+    if (!userId) {
+      return res.status(400).json({ message: "Invalid input" });
+    }
+
     const user = await User.findByPk(userId);
-    res.status(200).json(user);
-  } catch (err) {
-    console.error(err);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
   }
 };
 
 module.exports = {
   createUser,
   getUsers,
-  getUser,
+  getUserByUserId,
 };
